91ea5cf8e0a6e63579f470a4c4be32769f3cfd7b
alabador <austinlabador@gmail.com>
2022-09-28 15:12:33 -0700

    Add opposite number challenge

---
+++ 8kyu/opposite-number.js
@@ -0,0 +1,14 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+/*** My Solution***/
+function opposite(number) {
+    if (number > 0) return (-Math.abs(number))
+    else return Math.abs(number);
+}
