83d4f4daed7b3e692ac03f93413fb804351a419d
alabador <austinlabador@gmail.com>
2022-10-31 01:54:21 -0700

    Complete challenges

--- 8kyu/if-else-ternary.js
+++ 8kyu/if-else-ternary.js
@@ -5,3 +5,9 @@
 // n >= 5 and n < 10	95
 // n >= 10	90
 // You can use if..else or ternary operator to complete it.
+
+function saleHotdogs(n){
+    if (n < 5) return 100 * n;
+    else if (n >= 5 && n < 10) return 95 * n;
+    else if (n >= 10) return 90 * n;
+  }
