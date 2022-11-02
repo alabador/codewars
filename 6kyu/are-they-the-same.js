f3300391862319310fb8a4b3e2edf6d3f81f2591
alabador <austinlabador@gmail.com>
2022-11-01 23:14:05 -0700

    Add 6kyu challenge

---
+++ 6kyu/are-they-the-same.js
@@ -0,0 +1,29 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+
+/* My Solution */
+
+function comp(array1, array2){
+    if(array1 === null || array2 === null) return false;
+    const squared = array1.map(x => x*x);
+    for (let i = 0; i < squared.length; i++){
+      const containsSquare = array2.includes(squared[i]);
+      if (containsSquare === true){
+        const indexTwo = array2.findIndex(x => x === squared[i]);
+        array2.splice(indexTwo, 1)
+      }
+      else if (containsSquare === false){
+        return false;
+      }
+    }
+    return true;
+  }
