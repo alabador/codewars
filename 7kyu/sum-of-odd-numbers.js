2db65dfcc9ac7344d283efe2f6a2a4cf9a1a2a2d
alabador <austinlabador@gmail.com>
2022-11-10 22:04:20 -0800

    Add 7kyu challenge

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,10 @@
+// Given the triangle of consecutive odd numbers:
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+function rowSumOddNumbers(n) {
+	return n * n * n
+}
+
+//this solution is a more pattern recognition, math based type of question. 
+//At least, to get a cleaner solution it is.
