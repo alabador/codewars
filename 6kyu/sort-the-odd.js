20c14a9210e2ff2f704f28b65c9cb3bf12379233
alabador <austinlabador@gmail.com>
2022-12-05 21:51:29 -0800

    1 test failed, mostly solved

--- 6kyu/sort-the-odd.js
+++ 6kyu/sort-the-odd.js
@@ -3,3 +3,18 @@
 
 // [7, 1]  =>  [1, 7]
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+
+function sortArray(array) {
+    const odd = array.filter(x => x%2 !== 0) //get odd numbers
+    let oddIndex = [];
+    //find index of each odd number in array
+    for (let i=0; i<odd.length; i++){
+      oddIndex.push(array.indexOf(odd[i], i)); 
+    }
+    odd.sort((a,b) => a-b);
+    //push sorted values to saved indexes
+    for (let i=0; i<oddIndex.length; i++){
+      array.splice(oddIndex[i], 1, odd[i]);
+    }
+    return array;
+  }
